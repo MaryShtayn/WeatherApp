88511b10e7e30c16118e33fd93c83413976a523e
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2022-08-03 14:03:09 +0200

    added HTML, CSS, JS

---
+++ src/script.js
@@ -0,0 +1,54 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thusday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours} : ${minutes}`;
+}
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#search-input");
+  cityElement.innerHTML = cityInput.value;
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+//Challenge 1
+let dateElement = document.querySelector("#today");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+//Challenge 2
+let searchFoarm = document.querySelector("#search-form");
+searchFoarm.addEventListener("submit", search);
+
+//Challenge 3
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
